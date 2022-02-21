d5504d5127a525b026b8e1beacfe032fa2f77d5e
Dragon <dragon@email.com>
2022-02-21 12:29:46 +0700

    backend update

--- Backend/conduit/apps/devices/views.py
+++ Backend/conduit/apps/devices/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
+from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework import serializers, status, viewsets
 from rest_framework.exceptions import NotFound
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
 import logging
 
 from .models import Device
 from .serializers import DeviceSerializer
 
 logger = logging.getLogger(__name__)
+
 
 class AddDeviceAPIView(APIView):
     # Allow any user (authenticated or not) to hit this endpoint.
@@ -29,10 +32,40 @@ class AddDeviceAPIView(APIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-class DeviceAPIView(APIView):
+class GetDeviceListView(generics.ListCreateAPIView):
+    permission_classes = (AllowAny,)
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
 
-     def get(self, request):
-        return JsonResponse({'some': 'data'})
+    def list(self, request):
+        # Note the use of `get_queryset()` instead of `self.queryset`
+        queryset = self.get_queryset()
+        serializer = DeviceSerializer(queryset, many=True)
+        return Response(serializer.data)
+class GetDeviceView(RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = DeviceSerializer
+    queryset = Device.objects.all()
 
+    def retrieve(self, request, id, *args, **kwargs):
+        try:
+            device = Device.objects.get(id=id)
+        except device.DoesNotExist:
+            raise NotFound('A device with this id does not exist.')
 
-        
+        serializer = self.serializer_class(device, context={
+            'request': request
+        })
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    def delete(self, request, id):
+        try:
+            device = Device.objects.get(id=id)
+            device.delete()        
+            queryset = self.get_queryset()    
+        except device.DoesNotExist:
+            raise NotFound('A device with this id does not exist.')
+
+        serializer = DeviceSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
